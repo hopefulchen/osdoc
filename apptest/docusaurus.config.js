8148bcdc34896774f69398fbb75562681d33ebe3
v_chenhao05 <v_chenhao05@baidu.com>
2023-06-27 16:07:00 +0800

    修改链接

--- apptest/docusaurus.config.js
+++ apptest/docusaurus.config.js
@@ -40,15 +40,9 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/hopefulchen/osdoc/tree/master/apptest/',
         },
-        blog: {
-          showReadingTime: false,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+       
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -74,7 +68,7 @@ const config = {
             label: 'Docs',
           },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/hopefulchen/osdoc',
             label: 'GitHub',
             position: 'right',
           },
